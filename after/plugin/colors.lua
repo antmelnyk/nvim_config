fb15dc93cefe4f703899c70d273f449dfc16be19
Anton Melnyk <anton.melnyk@printify.com>
2023-03-03 06:09:05 +0200

    Color scheme plus search fix

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,16 +1,6 @@
-require('rose-pine').setup({
-  disable_background = true
-})
-
-function ColorMyPencils(color)
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
-
-  vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+function ColorMyPencils()
+  vim.opt.termguicolors = true
+  vim.cmd.colorscheme 'melange'
 end
 
 ColorMyPencils()
-
